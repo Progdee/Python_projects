68200b8e39c58c8ff72bbd2c3bc8ea71c1337dcf
Progdee <dhebhyofummite1@yahoo.com>
2023-03-28 23:24:58 +0100

    I'm improving

--- Projects/Collatzs_conjecture.py
+++ Projects/Collatzs_conjecture.py
@@ -1,20 +1,53 @@
+#This is an iteration that gives 1 as the final result no matter what number
+#you input.
+
+#ask the user to input a natural number
 c0 = int(input("Enter a natural number: "))
-while c0 != 1:
-    if c0 % 2 == 0:
-        c0 = c0 // 2
-        print(c0)
+if c0 < 1:
+    print("Please, enter a natural number: ")
+    c0 = int(input())
+    if c0 < 1:
+    print("Please, enter a natural number: ")
+    c0 = int(input())
+else:
+    while c0 != 1:
         if c0 % 2 == 0:
             c0 = c0 // 2
             print(c0)
-        else: 
+            if c0 % 2 == 0:
+                c0 = c0 // 2
+                print(c0)
+            else: 
+                c0 = 3 * c0 + 1
+                print(c0)
+        else:
             c0 = 3 * c0 + 1
             print(c0)
-    else:
-        c0 = 3 * c0 + 1
-        print(c0)
+            if c0 % 2 == 0:
+                c0 = c0 // 2
+                print(c0)
+            else: 
+                c0 = 3 * c0 + 1
+                print(c0)
+
+else:
+    while c0 != 1:
         if c0 % 2 == 0:
             c0 = c0 // 2
             print(c0)
-        else: 
+            if c0 % 2 == 0:
+                c0 = c0 // 2
+                print(c0)
+            else: 
+                c0 = 3 * c0 + 1
+                print(c0)
+        else:
             c0 = 3 * c0 + 1
             print(c0)
+            if c0 % 2 == 0:
+                c0 = c0 // 2
+                print(c0)
+            else: 
+                c0 = 3 * c0 + 1
+                print(c0)
+    
